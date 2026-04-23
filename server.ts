26fb1df4977f383ba88ff92b7b6942893a513ba2
mohit-180 <mohitgoswammi195@gmail.com>
2026-04-23 23:15:07 +0530

    separate frontend and backend tsconfig

--- server.ts
+++ server.ts
@@ -3,14 +3,15 @@ import { createServer as createViteServer } from "vite";
 import path from "path";
 
 const __dirname = path.resolve();
+const app = express();
 
 app.use(express.static(path.join(__dirname, "dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+
+const PORT = Number(process.env.PORT) || 5000;
 
 app.use(express.json());
 
